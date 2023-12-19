455e2edc72bcfc166ceb8b249d7a26b67873f2ef
Jay Lee <leejayjong@gmail.com>
2023-12-19 11:08:51 -0800

    feat: add codings for github copilot test

--- convert.py
+++ convert.py
@@ -1,42 +1,42 @@
 import os
-import yaml
 import eyed3
-import time
+import yaml
+from datetime import timedelta
 
-# Get all audio files in the 'audio' directory
+# This function takes a file name, loads the audio file, extracts the necessary information,
+# and returns it as a dictionary.
+def get_audio_file_info(file):
+    # Construct the full path of the file
+    audio_file_path = os.path.join('audio', file)
+    # Load the audio file
+    audio_file = eyed3.load(audio_file_path)
+    # Extract and join all comments in the audio file
+    comments = ' '.join(comment.text for comment in audio_file.tag.comments)
+    # Format the duration of the audio file
+    duration = format_duration(audio_file.info.time_secs)
+    # Get the size of the file in bytes and format it with commas as thousand separators
+    size = format(os.path.getsize(audio_file_path), ',')
+    # Return the extracted information as a dictionary
+    return {'title': audio_file.tag.title, 'description': comments, 'file': '/audio/' + file, 'duration': duration, 'length': size}
+
+# This function takes a duration in seconds and returns it as a string in the format HH:MM:SS.
+def format_duration(seconds):
+    return str(timedelta(seconds=int(seconds)))
+
+# This function lists all .mp3 files in the 'audio' directory and returns their information as a list of dictionaries.
 def get_audio_files():
     audio_files = []
     for file in os.listdir('audio'):
         if file.endswith('.mp3'):
-            # Load the audio file using the eyed3 library
-            audio_file = eyed3.load(os.path.join('audio', file))
-            comments = ''
-            # Get all comments in the audio file
-            if audio_file.tag.comments:
-                for comment in audio_file.tag.comments:
-                    comments += comment.text
-            # Get the duration of the audio file
-            duration = time.strftime('%H:%M:%S', time.gmtime(audio_file.info.time_secs))
-            # Get the size of the audio file
-            size = os.path.getsize(os.path.join('audio', file))
-            size_str = '{:,}'.format(size)
-            # Add the audio file information to the list
-            audio_files.append({
-                'title': audio_file.tag.title,
-                'description': comments,
-                'file': '/audio/' + file,
-                'duration': duration,
-                'length': size_str
-            })
+            audio_files.append(get_audio_file_info(file))
     return audio_files
 
-# Convert the audio files to YAML format
-def convert_to_yaml():
-    data = get_audio_files()
-    yaml_data = yaml.dump(data, sort_keys=False)
-    # Write the YAML data to a file
-    with open('episodes.yaml', 'w') as f:
-        f.write(yaml_data)
+# This function writes the information of all audio files to a YAML file.
+def main():
+    with open('episodes.yaml', 'w') as file:
+        yaml.dump(get_audio_files(), file,
+                  default_flow_style=False, sort_keys=False)
 
-# Call the convert_to_yaml function
-convert_to_yaml()
+# If this script is run as the main program, execute the main function.
+if __name__ == "__main__":
+    main()
